1d1834b5166a9a7c09d4fe57d72c9ee73afca346
Jan Palmer <jpalm@eg.dk>
2023-12-30 13:37:15 +0100

    Change App Icon

--- SmartHome/SmartHome/SmartHome.Android/MainActivity.cs
+++ SmartHome/SmartHome/SmartHome.Android/MainActivity.cs
@@ -5,7 +5,7 @@ using Android.Runtime;
 
 namespace SmartHome.Droid
 {
-    [Activity(Label = "SmartHome", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize)]
+    [Activity(Label = "SmartHome", Icon = "@mipmap/SmartHomeAppIcon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
